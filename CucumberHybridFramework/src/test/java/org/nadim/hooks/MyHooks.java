176d3c7af4493f01fb4c4f8098cb27bc043854b8
Nadim mostafa <nadimmostafa26@gmail.com>
2024-09-17 11:49:35 +0600

    Add Screenshot in HTMl Report if Test Case is failed

--- CucumberHybridFramework/src/test/java/org/nadim/hooks/MyHooks.java
+++ CucumberHybridFramework/src/test/java/org/nadim/hooks/MyHooks.java
@@ -1,13 +1,17 @@
 package org.nadim.hooks;
 
+import java.io.File;
 import java.util.Properties;
 
 import org.nadim.base.Base;
 import org.nadim.utility.ConfigProperty;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 public class MyHooks extends Base {
 	private WebDriver driver;
@@ -21,7 +25,14 @@ public class MyHooks extends Base {
 	}
 
 	@After
-	public void tearDown() {
+	public void tearDown(Scenario scenario) {
+		
+		String scenarioName = scenario.getName().replace(" ", "");
+		
+		if(scenario.isFailed()) {
+			byte[] screenshotAs = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+			scenario.attach(screenshotAs, "image/png", scenarioName);
+		}
 		driver.close();
 	}
 }
